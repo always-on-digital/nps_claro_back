dc176461354c03538a6fb06bc1b16039dfb5fd83
anselmo-alwayson <anselmo.elpidio@aodigital.com.br>
2026-02-12 11:51:07 -0300

    Configure CORS middleware with specific options
    Permitir acesso por todos os métodos e origens

--- app.js
+++ app.js
@@ -16,7 +16,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: false
+}));
 app.use(logger('dev'));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: false, limit: '50mb' }));
